4c1cac31026e089b9680347e754540480da49514
Luis C. Garcia-Peraza Herrera <luiscarlos.gph@gmail.com>
2023-02-11 23:46:04 +0000

    Update cloudflare_dns_update.sh

--- src/cloudflare_dns_update.sh
+++ src/cloudflare_dns_update.sh
@@ -13,9 +13,17 @@ EMAIL=<your_email>
 # Domain name you want to edit
 ZONE_NAME=<your_domain>
 DNS_RECORD=<your_subdomain>
+DOMAIN_NAME="$DNS_RECORD.$ZONE_NAME"
 
 # API token with powers to edit the DNS configuration of the domain
 API_TOKEN=<your_api_token>
 
+# Check that jq is installed
+check_jq=$(which jq)
+if [ -z "${check_jq}" ]; then
+    echo -e "\033[0;31m [-] jq is not installed. To install it run: 'sudo apt install jq'."
+    exit 1
+fi
 
-
+# Check that that the domain name exists
+check_record_ipv4=$(dig -t a +short ${DOMAIN_NAME} | tail -n1)
