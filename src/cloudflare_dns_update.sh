17b784aa060b6834a625ea812c5edf5c3a22b1eb
Luis C. Garcia-Peraza Herrera <luiscarlos.gph@gmail.com>
2023-02-11 23:54:28 +0000

    Update cloudflare_dns_update.sh

--- src/cloudflare_dns_update.sh
+++ src/cloudflare_dns_update.sh
@@ -19,11 +19,27 @@ DOMAIN_NAME="$DNS_RECORD.$ZONE_NAME"
 API_TOKEN=<your_api_token>
 
 # Check that jq is installed
-check_jq=$(which jq)
-if [ -z "${check_jq}" ]; then
+CHECK_JQ=$(which jq)
+if [ -z "${CHECK_JQ}" ]; then
     echo -e "\033[0;31m [-] jq is not installed. To install it run: 'sudo apt install jq'."
     exit 1
 fi
 
 # Check that that the domain name exists
-check_record_ipv4=$(dig -t a +short ${DOMAIN_NAME} | tail -n1)
+CHECK_DOMAIN_EXISTS=$(dig -t a +short ${DOMAIN_NAME} | tail -n1)
+if [ -z "${CHECK_DOMAIN_EXISTS}" ]; then
+    echo -e "\033[0;31m [-] cannot retrieve the current IP address for the provided domain name. Are you sure that ${DOMAIN_NAME} exists?"
+    exit 1
+fi
+
+# Check that the API token is working
+USER_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" -H "Authorization: Bearer $api_token" -H "Content-Type:application/json" | jq -r '{"result"}[] | .id')
+# TODO: check what happens here when the token is wrong, what is the value of USER_ID?
+
+# Retrive your current IP address
+CURRENT_IP=$(curl -s -X GET -4 https://ifconfig.co)
+
+
+
+
+
