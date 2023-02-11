44fe31bd24ca331f7433d0b56e7f36f3163f5cf9
Luis C. Garcia-Peraza Herrera <luiscarlos.gph@gmail.com>
2023-02-11 23:30:39 +0000

    Update cloudflare_dns_update.sh

--- src/cloudflare_dns_update.sh
+++ src/cloudflare_dns_update.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+#
+# @brief    Script to update the IP address of a domain managed with Cloudflare.
+#
+# @details  Code inspired by: https://gist.github.com/Tras2/cba88201b17d765ec065ccbedfb16d9a
+#
+# @author   Luis C. Garcia-Peraza Herrera (luiscarlos.gph@gmail.com).
+# @date     11 Feb 2023.
+
+# E-mail associated with your Cloudflare account
+EMAIL=<your_email>
+
+# Domain name you want to edit
+ZONE_NAME=<your_domain>
+DNS_RECORD=<your_subdomain>
+
+# API token with powers to edit the DNS configuration of the domain
+API_TOKEN=<your_api_token>
+
+
 
