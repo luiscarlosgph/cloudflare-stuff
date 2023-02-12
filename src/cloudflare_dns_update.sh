d0801808560387800536bc77d24461d9c9f27bbf
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2023-02-12 03:53:19 +0000

    Script to update the DNS A record in Cloudflare is now working.

--- src/cloudflare_dns_update.sh
+++ src/cloudflare_dns_update.sh
@@ -8,38 +8,57 @@
 # @date     11 Feb 2023.
 
 # E-mail associated with your Cloudflare account
-EMAIL=<your_email>
+EMAIL="<email_here>"
 
 # Domain name you want to edit
-ZONE_NAME=<your_domain>
-DNS_RECORD=<your_subdomain>
-DOMAIN_NAME="$DNS_RECORD.$ZONE_NAME"
+ZONE_NAME="<domain_here>"
+DNS_RECORD="<subdomain_here>"
+DOMAIN_NAME="${DNS_RECORD}.${ZONE_NAME}"
 
 # API token with powers to edit the DNS configuration of the domain
-API_TOKEN=<your_api_token>
+API_TOKEN="<api_token_here>"
 
 # Check that jq is installed
 CHECK_JQ=$(which jq)
 if [ -z "${CHECK_JQ}" ]; then
-    echo -e "\033[0;31m [-] jq is not installed. To install it run: 'sudo apt install jq'."
+    echo -e "\033[0;31m [ERROR] jq is not installed. To install it run: 'sudo apt install jq'."
     exit 1
 fi
 
 # Check that that the domain name exists
 CHECK_DOMAIN_EXISTS=$(dig -t a +short ${DOMAIN_NAME} | tail -n1)
 if [ -z "${CHECK_DOMAIN_EXISTS}" ]; then
-    echo -e "\033[0;31m [-] cannot retrieve the current IP address for the provided domain name. Are you sure that ${DOMAIN_NAME} exists?"
+    echo -e "\033[0;31m [ERROR] cannot retrieve the current IP address for the provided domain name. Are you sure that ${DOMAIN_NAME} exists?"
     exit 1
 fi
 
 # Check that the API token is working
 USER_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type:application/json" | jq -r '{"result"}[] | .id')
-# TODO: check what happens here when the token is wrong, what is the value of USER_ID?
+if [ ${USER_ID} = "null" ]; then
+    echo -e "\033[0;31m [ERROR] the API token is wrong."
+    exit 1
+fi
 
 # Retrive your current IP address
 CURRENT_IP=$(curl -s -X GET -4 https://ifconfig.co)
+echo -e "\033[0;37m [INFO] External IP address: ${CURRENT_IP}"
 
+# Get the IP stored in Cloudflare
+ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE_NAME&status=active" -H "Content-Type: application/json" -H "X-Auth-Email: $EMAIL" -H "Authorization: Bearer $API_TOKEN" | jq -r '{"result"}[] | .[0] | .id')
+echo -e "\033[0;37m [INFO] Cloudflare zone id: ${ZONE_ID}"
+DNS_RECORD_A_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=${DOMAIN_NAME}" -H "Content-Type: application/json" -H "X-Auth-Email: $EMAIL" -H "Authorization: Bearer $API_TOKEN")
+DNS_RECORD_A_IP=$(echo $DNS_RECORD_A_ID |  jq -r '{"result"}[] | .[0] | .content')
+echo -e "\033[0;37m [INFO] DNS record id: ${DNS_RECORD_A_IP}"
 
-
-
-
+# Update the record if it needs to be updated 
+if [ $DNS_RECORD_A_IP != $CURRENT_IP ]; then
+    echo -e "\033[0;37m [INFO] The record needs to be updated."
+    RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$(echo $DNS_RECORD_A_ID | jq -r '{"result"}[] | .[0] | .id')" -H "Content-Type: application/json" -H "X-Auth-Email: $EMAIL" -H "Authorization: Bearer $API_TOKEN" --data "{\"type\":\"A\",\"name\":\"$DNS_RECORD\",\"content\":\"$CURRENT_IP\",\"ttl\":1,\"proxied\":true}" | jq -r '.errors')
+    if [ "${RESPONSE}" = "[]" ]; then
+        echo -e "\033[0;37m [INFO] The DNS record for ${DOMAIN_NAME} has been successfully updated."
+    else
+        echo -e "\033[0;31m [ERROR] The DNS record update for ${DOMAIN_NAME} failed."
+    fi
+else
+    echo -e "\033[0;37m [INFO] The record does not need to be updated."
+fi
